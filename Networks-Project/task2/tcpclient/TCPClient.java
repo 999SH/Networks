44993b6868230cdc04e559c8da00cdd666b814fe
999SH <viktor.salomonsson12@gmail.com>
2023-02-09 18:38:31 +0100

    Project task 2 almost done

--- Networks-Project/task2/tcpclient/TCPClient.java
+++ Networks-Project/task2/tcpclient/TCPClient.java
@@ -5,66 +5,64 @@ import java.io.*;
 public class TCPClient {
     static int BUFFERSIZE = 1024;
     
+    boolean shutdown;  //Declare all variable as "global" so that they can be used within the scope of askserver despite getting their value in tcpclient
+    Integer timeout;   //Wrapper class that can handle null
+    Integer limit;
+
+    
     public TCPClient(boolean shutdown, Integer timeout, Integer limit) {
+        if (timeout != null){
+            this.timeout = timeout; //incase of failed null recognition it is prefereable to have no timeout rather than the client breaking at socket.setSoTimeout
+        } else {
+            this.timeout = 0;       //0 means no timeout
+        }
+
+        if (limit != null){
+            this.limit = limit;        
+        } else {
+            this.limit = Integer.MAX_VALUE;  //If null then there is no limit 
+        }
+
+        this.shutdown = shutdown;       //Default is false, should only matter if true. 
+
     }
 
     public byte[] askServer(String hostname, int port, byte [] toServerBytes) throws IOException {
 
-            Socket socket = new Socket(hostname, port);
-
-            try {
-
-                OutputStream outputStream = socket.getOutputStream();                //Creates the outputstream used to write data to a socket
-
-                outputStream.write(toServerBytes);                                   //Output toserverbytes array to server socket bytearray
-
-                InputStream inputStream = socket.getInputStream();                  //Initalize the inputstream used to read the response from the server
-
-                ByteArrayOutputStream bytearrayout = new ByteArrayOutputStream();   //Initialize byteArr outputstream, BAOS is a dynamically allocated array
-
-                byte[] fixedfromServerBuffer = new byte[BUFFERSIZE];                //Initial fixed in-memory buffer
-
-                int length;                                                         //Changes each iteration of the while loop, represents the number of bytes read in the buffer
-
-                while ((length = inputStream.read(fixedfromServerBuffer)) != -1) {   //Reads 1024 bytes per loop iteration into the buffer
-                    bytearrayout.write(fixedfromServerBuffer, 0, length);            //Inputstream.read returns -1 when there is no more data to read
-                }                                                                    //used to store the response of the server even if bigger than 1024 bytes
-                return bytearrayout.toByteArray();                                   //Return all of the data from the server in a single array
-
-            } catch(IOException error) {
-                System.err.println("Catch error" + error.getMessage());
-                throw error;
-
-            }finally {
-                socket.close();  //Ensure socket is always closed
+            Socket socket = new Socket(hostname, port);                         //Create the socket
+            OutputStream outputStream = socket.getOutputStream();               //Creates the outputstream used to write data to a socket
+            outputStream.write(toServerBytes);                                  //Output toserverbytes array to server socket bytearray
+            if (shutdown == true){
+                socket.shutdownOutput();
             }
 
-
-    }
-    public byte[] askServer(String hostname, int port) throws IOException {
-
-        Socket socket = new Socket(hostname, port);
-
-        try {
             InputStream inputStream = socket.getInputStream();                  //Initalize the inputstream used to read the response from the server
-
             ByteArrayOutputStream bytearrayout = new ByteArrayOutputStream();   //Initialize byteArr outputstream, BAOS is a dynamically allocated array
-
             byte[] fixedfromServerBuffer = new byte[BUFFERSIZE];                //Initial fixed in-memory buffer
-
             int length;                                                         //Changes each iteration of the while loop, represents the number of bytes read in the buffer
+            int totalLength = 0;
+            socket.setSoTimeout(timeout);                                       //Set timeout and try to read if not null, if 0 is set it removes the timeout limit. 
+    
+            try {
+                while (((length = inputStream.read(fixedfromServerBuffer)) != -1)&&(totalLength <= limit)) { 
+                bytearrayout.write(fixedfromServerBuffer, 0, length);
+                totalLength += length;                                                                   
+            }
 
-            while ((length = inputStream.read(fixedfromServerBuffer)) != -1) {   //Reads 1024 bytes per loop iteration into the buffer
-                bytearrayout.write(fixedfromServerBuffer, 0, length);            //Inputstream.read returns -1 when there is no more data to read
-            }                                                                    //used to store the response of the server even if bigger than 1024 bytes
-            return bytearrayout.toByteArray();                                   //Return all of the data from the server in a single array
+            return bytearrayout.toByteArray(); 
+            } catch(SocketException error) {
+                System.err.println("Socket timeout reached" +error.getMessage());
+                throw error;
 
-        } catch(IOException error) {
-            System.err.println("Catch error" + error.getMessage());
-            throw error;
-
-        }finally {
-            socket.close();  //Ensure socket is always closed
+            } catch(IOException error) {
+                System.err.println("IO error" + error.getMessage());
+                throw error;
+                
+            }finally { 
+                socket.close();  //Ensure socket is always closed
+            }
         }
+        public byte[] askServer(String hostname, int port) throws IOException { //incase no toserverbytes
+            return askServer(hostname, port, new byte[0]);
     }
 }
